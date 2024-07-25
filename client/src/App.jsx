535840d53cebbaeed192bf0ec11fe414dab2310b
Samuel Teigland <samuelteigland@websiteartificers.com>
2024-07-25 10:46:27 -0500

    fixed GA

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,15 @@
 import { Hero, Fix, Review, Benefits, Services, Pricing, ContactUs, Facebook } from './components/components';
+import ReactGA from 'react-ga4';
 
 export default function App() {
+    ReactGA.initialize('G-BY6RR52CSJ');
+
+    ReactGA.send({
+        hitType: "pageview",
+        page: "/",
+        title: "Home Page",
+    })
+
     return (
         <div className='home__container'>
             <Facebook />
