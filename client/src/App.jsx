4d8966d438646722517ea762c780be5d217db758
Samuel Teigland <samuelteigland@websiteartificers.com>
2024-07-25 11:55:20 -0500

    added speed insights

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Hero, Fix, Review, Benefits, Services, Pricing, ContactUs, Facebook, Google } from './components/components';
 import ReactGA from 'react-ga4';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App() {
     ReactGA.initialize('G-BY6RR52CSJ');
@@ -13,6 +14,7 @@ export default function App() {
 
     return (
         <div className='home__container'>
+            <SpeedInsights />
             <Analytics />
             <Google />
             <Facebook />
