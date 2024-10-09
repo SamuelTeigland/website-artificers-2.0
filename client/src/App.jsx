f1f29bce31191f6dd0fb2bfe7ef82d66e8cd7cf4
Samuel Teigland <samuelteigland@websiteartificers.com>
2024-10-09 11:48:39 -0500

    maybe?

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import PrivacyPage from './PrivacyPage';
 
 export default function App() {
 
     return (
-        <BrowserRouter>
+        <Router>
             <Routes>
                 <Route exact path="/" element={<HomePage />} />
                 <Route path="/privacy" element={<PrivacyPage />} />
             </Routes>
-        </BrowserRouter>
+        </Router>
     )
 }
